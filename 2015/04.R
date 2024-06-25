62675343b5a44938adbc94f3a19e098e6a30a031
Jacob <57509628+Asaranth@users.noreply.github.com>
2024-06-25 10:43:20 +0100

    Solved 2015 day 4

--- 2015/04.R
+++ 2015/04.R
@@ -7,17 +7,17 @@ close(con)
 
 solvePartOne <- function() {
   number <- seq_len(400000)
-  md5 <- digest::getVDigest()
+  md5 <- getVDigest()
   hash <- vapply(paste0(data, number), md5, FUN.VALUE=character(1), serialize=FALSE)
   return(which(substr(hash, 1, 5) == "00000"))
 }
 
 solvePartTwo <- function() {
   number <- seq(9000000, 10000000)
-  md5 <- digest::getVDigest()
+  md5 <- getVDigest()
   hash <- vapply(paste0(data, number), md5, FUN.VALUE=character(1), serialize=FALSE)
   return(number[[which(substr(hash, 1, 6) == "000000")]])
 }
 
-#str_interp('Part One: ${solvePartOne()}')
+str_interp('Part One: ${solvePartOne()}')
 str_interp('Part Two: ${solvePartTwo()}')
