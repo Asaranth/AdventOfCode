d9546b645881e5b52aa27c97000d0197a9c4570e
Jacob Fairhead <57509628+Asaranth@users.noreply.github.com>
2024-10-11 16:51:11 +0100

    Update README section title
    Renamed the "Years and Languages" section to just "Years" for clarity. This change simplifies the section title to better reflect its content.

--- update_readme.py
+++ update_readme.py
@@ -95,7 +95,7 @@ def main(args):
 
 Advent of Code is a delightful online event created by Eric Wastl. It's a coding celebration that happens every December, treating you to daily programming puzzles from the 1st to the 25th. Join the fun at [Advent of Code](https://adventofcode.com/)!
 
-## Years and Languages
+## Years
 
 {year_lines}
 """
