67b0ace49b2497e7be65e4ed084d0b3fabca6dc2
Jacob <57509628+Asaranth@users.noreply.github.com>
2024-10-18 15:48:32 +0100

    Add solution for 2015 Day 23

--- 2015/23.R
+++ 2015/23.R
@@ -5,33 +5,33 @@ execute <- function(a, b) {
   pc <- 1
   while (pc <= length(data) && pc > 0) {
     instruction <- data[pc]
-    parts <- unlist(strsplit(instruction, " "))
+    parts <- unlist(strsplit(instruction, ' '))
     cmd <- parts[1]
-    if (cmd %in% c("hlf", "tpl", "inc")) {
+    if (cmd %in% c('hlf', 'tpl', 'inc')) {
       reg <- substr(parts[2], 1, 1)
-      if (cmd == "hlf") {
-        if (reg == "a") a <- a / 2 else b <- b / 2
-      } else if (cmd == "tpl") {
-        if (reg == "a") a <- a * 3 else b <- b * 3
-      } else if (cmd == "inc") {
-        if (reg == "a") a <- a + 1 else b <- b + 1
+      if (cmd == 'hlf') {
+        if (reg == 'a') a <- a / 2 else b <- b / 2
+      } else if (cmd == 'tpl') {
+        if (reg == 'a') a <- a * 3 else b <- b * 3
+      } else if (cmd == 'inc') {
+        if (reg == 'a') a <- a + 1 else b <- b + 1
       }
       pc <- pc + 1
-    } else if (cmd == "jmp") {
+    } else if (cmd == 'jmp') {
       offset <- as.numeric(parts[2])
       pc <- pc + offset
-    } else if (cmd == "jie" || cmd == "jio") {
+    } else if (cmd == 'jie' || cmd == 'jio') {
       reg <- substr(parts[2], 1, 1)
       offset <- as.numeric(parts[3])
-      if (cmd == "jie" && ((reg == "a" && a %% 2 == 0) || (reg == "b" && b %% 2 == 0))) {
+      if (cmd == 'jie' && ((reg == 'a' && a %% 2 == 0) || (reg == 'b' && b %% 2 == 0))) {
         pc <- pc + offset
-      } else if (cmd == "jio" && ((reg == "a" && a == 1) || (reg == "b" && b == 1))) {
+      } else if (cmd == 'jio' && ((reg == 'a' && a == 1) || (reg == 'b' && b == 1))) {
         pc <- pc + offset
       } else {
         pc <- pc + 1
       }
     } else {
-      stop("Unknown instruction: ", cmd)
+      stop('Unknown instruction: ', cmd)
     }
   }
   return(b)
