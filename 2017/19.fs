c6a9782b8a24155c17de0d0bdf0616a1df17ae89
Jacob <57509628+Asaranth@users.noreply.github.com>
2024-11-11 09:23:46 +0000

    Added solution for 2017 day 19

---
+++ 2017/19.fs
@@ -0,0 +1,52 @@
+﻿namespace _2017
+
+open System
+
+module _19 =
+    let Data = (Utils.GetInputData 19).Split('\n', StringSplitOptions.RemoveEmptyEntries)
+
+    let findStartPosition(diagram: string[]) = diagram[0].IndexOf('|')
+
+    let isLetter(c: char) = Char.IsLetter(c)
+
+    let traversePath(diagram: string[]) (onVisit: int -> int -> char -> unit) =
+        let rows = diagram.Length
+        let cols = diagram[0].Length
+        let mutable x = findStartPosition diagram
+        let mutable y = 0
+        let mutable direction = (0, 1)
+        let mutable continueWalking = true
+        onVisit x y diagram[y].[x]
+
+        while continueWalking do
+            let dx, dy = direction
+            x <- x + dx
+            y <- y + dy
+            if x < 0 || y < 0 || x >= cols || y >= rows || diagram[y].[x] = ' ' then continueWalking <- false
+            else
+                let charAtPos = diagram[y][x]
+                onVisit x y charAtPos
+                if charAtPos = '+' then
+                    direction <-
+                        if dx <> 0 then
+                            if y > 0 && diagram[y - 1][x] <> ' ' then (0, -1)
+                            else (0, 1)
+                        else
+                            if x > 0 && diagram[y][x - 1] <> ' ' then (-1, 0)
+                            else (1, 0)
+
+    let solvePartOne() =
+        let letters = System.Collections.Generic.List<char>()
+        let onVisit _ _ charAtPos = if isLetter charAtPos then letters.Add charAtPos
+        traversePath Data onVisit
+        new string (letters.ToArray())
+
+    let solvePartTwo() =
+        let mutable steps = 0
+        let onVisit _ _ _ = steps <- steps + 1
+        traversePath Data onVisit
+        steps
+
+    let Run() =
+        printfn $"Part One: {solvePartOne()}"
+        printfn $"Part Two: {solvePartTwo()}"
